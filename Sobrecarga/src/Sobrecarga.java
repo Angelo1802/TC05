2900d962367670d790e56610aed7660cf5b2dc8a
Alexis Aguirre <alexisaguirregamonal9@gmail.com>
2025-09-20 23:25:58 -0500

    Agrego mi proyecto Sobrecarga

---
+++ Sobrecarga/src/Sobrecarga.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author alexi
+ */
+public class Sobrecarga {
+    public static void main(String[] args) { 
+        SobrecargaDeMetodos obj = new SobrecargaDeMetodos(); 
+         obj.mostrarInfo("Juan"); 
+         obj.mostrarInfo(30); 
+         obj.mostrarInfo("Ana", 25); 
+    }
+}
